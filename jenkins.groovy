35923e7f2736292d79ccf11cfe144515563c22db
Paul Mosi <mailpaulmosi@gmail.com>
2025-10-21 17:51:34 +0100

    Add Jenkins pipeline configuration

---
+++ jenkins.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/officialpaul/web-app-project.git'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                echo 'Building...'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                echo 'Running tests...'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                echo 'Deploying...'
+            }
+        }
+    }
+}
