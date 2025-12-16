8c4e65e6130e619e7a9ce0fa03201a9b76f55532
Paul Mosi <mailpaulmosi@gmail.com>
2025-12-16 06:03:31 +0100

    Fix Docker login credentials variable usage

--- jenkins.groovy
+++ jenkins.groovy
@@ -43,8 +43,8 @@ pipeline {
         stage('Docker Login') {
             steps {
                 sh """
-                  echo "${DOCKERHUB_CREDENTIALS_PSW}" | \
-                  docker login -u "${DOCKERHUB_CREDENTIALS_USR}" --password-stdin
+                  echo "${dockerhub-creds}" | \
+                  docker login -u "${dockerhub-creds}" --password-stdin
                 """
             }
         }
